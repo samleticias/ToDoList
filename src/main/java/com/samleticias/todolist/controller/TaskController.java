ebf8f629dc420024470be7d1c8d51afdf2bcbeed
samleticias <sammyaleticia09@gmail.com>
2024-10-05 20:09:56 -0300

    feat: add method to create task in task controller

--- src/main/java/com/samleticias/todolist/controller/TaskController.java
+++ src/main/java/com/samleticias/todolist/controller/TaskController.java
@@ -4,10 +4,11 @@ import com.samleticias.todolist.entities.Task;
 import com.samleticias.todolist.repositories.TaskRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,10 +20,27 @@ public class TaskController {
     private TaskRepository taskRepository;
 
     @GetMapping
-    public List<Task> list(HttpServletRequest request){
+    public List<Task> getTasks(HttpServletRequest request){
         var idUser = request.getAttribute("idUser");
         var tasks = this.taskRepository.findByIdUser((UUID) idUser);
         return tasks;
     }
-    
+
+    @PostMapping
+    public ResponseEntity createTask(@RequestBody Task task, HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        task.setIdUser((UUID) idUser);
+
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(task.getStartAt()) || currentDate.isAfter(task.getFinishAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data inválida.");
+        }
+
+        if (task.getStartAt().isAfter(task.getFinishAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data inválida.");
+        }
+        var newTask = this.taskRepository.save(task);
+        return ResponseEntity.status(HttpStatus.OK).body(newTask);
+    }
+
 }
